499689eb10bff64ed29908a59218bf84f212b78d
suhyun han <amyhan258@naver.com>
2023-12-18 18:32:05 +0900

    tidyr

--- 1218r8_tidy.R
+++ 1218r8_tidy.R
@@ -43,4 +43,37 @@ ggplot(table1, aes(year, cases)) +
   geom_line(aes(group = country), colour = "grey50") + 
   geom_point(aes(colour = country))
 
-#Pivoting: 방향전환->tidy로 바꾸는 법
+#tibble을 tidy로 바꾸는 법
+#Pivoting: 방향전환
+table4a
+table4a %>% 
+  pivot_longer(c(`1999`, `2000`), names_to = "year", values_to = "cases")
+table2 %>%
+  pivot_wider(names_from = type, values_from = count)
+#쪼개거나 붙이기
+table3 %>% 
+  separate(rate, into = c("cases", "population"), sep = "/", convert = TRUE) # 걍 디폴트가 문자로 변환이라
+table5 %>% 
+  unite(new, century, year, sep = "")
+
+#missing values
+# Explicitly, i.e. flagged with NA.
+# Implicitly, i.e. simply not present in the data.
+stocks <- tibble(
+  year   = c(2015, 2015, 2015, 2015, 2016, 2016, 2016),
+  qtr    = c(   1,    2,    3,    4,    2,    3,    4),
+  return = c(1.88, 0.59, 0.35,   NA, 0.92, 0.17, 2.66)
+)
+stocks
+stocks %>% 
+  complete(year, qtr) #1
+
+treatment <- tribble(
+  ~ person,           ~ treatment, ~response,
+  "Derrick Whitmore", 1,           7,
+  NA,                 2,           10,
+  NA,                 3,           9,
+  "Katherine Burke",  1,           4,
+)
+treatment %>% 
+  fill(person)
